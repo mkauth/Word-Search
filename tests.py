14c5cd88ef670268aa48949a2556cd56533b4ee4
Matthew Kauth <kauthmatt@gmail.com>
2021-03-28 21:27:34 -0500

    Fixed typo in testcase setup name. Added test for loading word search from file. Added functionality for loading word search from file.

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import unittest
 
 class WordSearchTestCase(unittest.TestCase):
 
-	def setup(self):
+	def setUp(self):
 		self.test_grid = [
 			['E','O','H','D','A'],
 			['T','H','O','M','C'],
@@ -11,3 +11,13 @@ class WordSearchTestCase(unittest.TestCase):
 			['R','A','E','B','T'],
 			['W','O','L','F','N']
 		]
+	
+	def test_load_grid_from_file(self):
+		words, grid = search.load_grid("testwordsearch.txt")
+		self.assertEqual(words, ['BEAR','CAT','DOG','WOLF'])
+		self.assertEqual(grid, self.test_grid)
+		
+		
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ wordsearch.py
@@ -0,0 +1,28 @@
+
+
+def load_grid(filename):
+	file = open(filename)
+	
+	#first line is word list, load them into variable
+	first_line = file.readline()
+	
+	#remove trailing new line
+	first_line = first_line.rstrip("\n")
+	
+	words = first_line.split(',')
+	
+	grid = []
+	#remaining lines are the grid, loop through and put in 2d list
+	for line in file:
+		row=[]
+		
+		#remove trailing new line from string
+		formatted_line = line.rstrip("\n")
+		
+		for character in formatted_line.split(','):
+			row.append(character)
+			
+		grid.append(row)
+		
+	file.close()
+	return words, grid
